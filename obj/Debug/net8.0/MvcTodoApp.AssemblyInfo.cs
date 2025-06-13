0262cca53c437b658c7cba87703e756841d5f2ad
C1T03 <msy47460@gmail.com>
2025-06-13 22:13:33 +0300

    Added Edit Task feature and Edit.cshtml view

--- obj/Debug/net8.0/MvcTodoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d11827d78b4d2474b487676073eec681219dc30d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
